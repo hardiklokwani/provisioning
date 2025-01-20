367ee471e3db11f45d77d57691b8715bb094bd21
hardiklokwani <lokwanihardik@gmail.com>
2025-01-20 09:34:31 +0000

    Terrform conf for proxmox vm using a clone template

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+        }
+    }
+}
+
+provider "proxmox" {
+    pm_api_url          = "https://192.168.178.200:8006///api2/json"
+    pm_api_token_id     = "yourtokenid"
+    pm_api_token_secret = "yourtokensecret"
+    pm_tls_insecure     = true
+}
+
+resource "proxmox_vm_qemu" "k3s-worker02" {
+    name                = "k3s-worker02"
+    target_node         = "hardik"
+    clone               = "hardik"
+    full_clone          = true
+    cores               = 4
+    memory              = 2048
+
+    disk {
+        size            = "128G"
+        type            = "scsi"
+        storage         = "local-lvm"
+        discard         = "on"
+    }
+
+    network {
+        model     = "virtio"
+        bridge    = "vmbr0"
+        firewall  = false
+        link_down = false
+    }
+
+}
